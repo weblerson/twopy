b5521724ad5c5881ad738f93e0b7e47b0c7ff95d
MarcRuas <peixoto52ruas@gmail.com>
2024-07-14 05:08:43 -0300

    feat: adicao das tabelas candidato, recrutador, empresa, vaga e aplicacao e seus relacionamentos

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
 from flask import Flask
+from models import db
 
 app = Flask('__name__')
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+
+db.init_app(app)
+
+with app.app_context():
+    db.create_all()
 
 if __name__ == '__main__':
     app.run(debug=True)
